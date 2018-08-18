4525387b9e02aef037501d0199e0cb55ab226505
Garett Anderson <garett.769@gmail.com>
2018-08-18 12:08:34 -0400

    Updated reset and comments

--- settings.py
+++ settings.py
@@ -3,26 +3,31 @@ class Settings:
         self.screen_width = 720
         self.screen_height = 356
         self.ground = self.screen_height - 20
-        self.bg_color = 227, 154, 171
+        self.bg_color = 227, 154, 171   # Playbutton
 
         self.play = False
-
+        # Cacti
         self.cactus_speed = 7
         self.cacti_allowed = 3
         self.score = 0
-
+        # Dino
         self.rotation = 0
         self.switch = 0
         self.frame = 0
-
+        # Fireball
         self.fireball = False
         self.gain_fb = False
         self.explode = False
         self.fireball_speed = 10
 
     def reset(self):
+        # Pauses game, resets score
         self.play = False
         self.score = 0
+        # So fireball can't be shot when restarting
+        self.fireball = False
+        self.gain_fb = False
+        # Tracks frames for dino, can I just control inside of dino?
         self.rotation = 0
         self.switch = 0
         self.frame = 0
